f44085a71b0d83fba0d002260f743c9eaa55c0e7
Jesse van der Pluijm <4806@pierson.nl>
2016-06-14 10:38:25 +0200

    using new setState() method
    Using the new setState() method in the app.js file..rather than the old method,

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
   grid.drawGrid();
   grid.updateDiameter(250);
@@ -6,18 +5,17 @@ $(document).ready(function() {
   var YX = grid.YX;
   YX[1][1].addClass('test');
 
-  grid.setState(0,2,2);
-  grid.setState(0,90);
+  grid.YX[2][2].setState(0)
+  grid.cells[90].setState(0)
 
   console.log('state1: '+grid.YX[2][2].state);
   console.log('state2: '+grid.cells[90].state);
 
   $(document).click(function() {
-    grid.setState(1, 2, 2)
-    grid.setState(1, 90)
+    grid.YX[2][2].setState(1)
+    grid.cells[90].setState(1)
     console.log('newState1: '+grid.YX[2][2].state);
     console.log('newState2: '+grid.cells[90].state);
   });
-
 
 });
