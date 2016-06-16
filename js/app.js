37fad689cc8324d331a6d94a60501f750aa31aa9
skillzzjesse <4806@pierson.nl>
2016-06-16 22:09:01 +0200

    added intervak

--- js/app.js
+++ js/app.js
@@ -6,8 +6,6 @@ $(document).ready(function() {
   //YX[1][1].addClass('test');
 
 
-
-
   function checkNeighbours(cell) {
     var x = cell.x;
     var y = cell.y;
@@ -123,13 +121,13 @@ $(document).ready(function() {
     }
   }
 
-  $(document).click(function(){
+  window.setInterval(function(){
     for (var i = 1; i < grid.amount*grid.amount+1; i++) {
       checkNeighbours(grid.cells[i]);
       nextStates(grid.cells[i]);
     }
     gameStep();
-  });
+  }, 175);
 
 
 });
