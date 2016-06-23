34375283a9f817eac944163732a05032143690ed
skillzzjesse <4806@pierson.nl>
2016-06-23 20:37:25 +0200

    Added the last functions
    Added the last functions and alterd them so they work with the new grid!
    
    everything seems to work!
    
    -jesse

--- js/alt.js
+++ js/alt.js
@@ -165,4 +165,126 @@ var grid = {
 
   }, // end
 
+  checkNeighbours: function(cell) {
+    var x = cell.x;
+    var y = cell.y;
+    var aliveNeighbours = 0;
+
+    // Determining the right neighbour.
+    if (x == this.amount) {
+      var right = this.YX[y][1];
+    }else{
+      var right = this.YX[y][x+1];
+    }
+    // Determining the left neighbour.
+    if (x == 1) {
+      var left = this.YX[y][this.amount];
+    }else{
+      var left = this.YX[y][x-1];
+    }
+    // Determining the top neighbour
+    if (y == 1) {
+      var top = this.YX[this.amount][x];
+    }else{
+      var top = this.YX[y-1][x];
+    }
+    // Determining the bottom neighbour.
+    if(y == this.amount){
+      var bottom = this.YX[1][x];
+    }else {
+      var bottom = this.YX[y+1][x];
+    }
+    // Determining top right neighbour.
+    if (top.x == this.amount) {
+      var topRight = this.YX[top.y][1];
+    }else{
+      var topRight = this.YX[top.y][top.x+1];
+    }
+    // Determining top left neighbour.
+    if(top.x == 1){
+      var topLeft = this.YX[top.y][this.amount];
+    }else {
+      var topLeft = this.YX[top.y][top.x-1]
+    }
+    // Determining bottom right neighbour.
+    if (bottom.x == this.amount) {
+      var bottomRight = this.YX[bottom.y][1]
+    }else {
+      var bottomRight = this.YX[bottom.y][bottom.x+1]
+    }
+    // Determining top left neighbour.
+    if(bottom.x == 1){
+      var bottomLeft = this.YX[bottom.y][this.amount];
+    }else {
+      var bottomLeft = this.YX[bottom.y][bottom.x-1]
+    }
+
+    // If a neighbours' state is alive the amount of alive neighbours =+1
+    if(right.state == 1){
+      aliveNeighbours = aliveNeighbours+1;
+    }
+    if (left.state == 1) {
+      aliveNeighbours = aliveNeighbours+1;
+    }
+    if(top.state == 1){
+      aliveNeighbours = aliveNeighbours+1;
+    }
+    if(bottom.state == 1){
+      aliveNeighbours = aliveNeighbours+1;
+    }
+    if(topRight.state == 1){
+      aliveNeighbours = aliveNeighbours+1;
+    }
+    if(topLeft.state == 1){
+      aliveNeighbours = aliveNeighbours+1;
+    }
+    if(bottomRight.state == 1){
+      aliveNeighbours = aliveNeighbours+1;
+    }
+    if(bottomLeft.state == 1){
+      aliveNeighbours = aliveNeighbours+1;
+    }
+    // Adding the amount of living neighbours to the cells object
+    cell.aliveNeighbours = aliveNeighbours;
+
+  }, // end
+
+  nextStates: function(cell) {
+    /*=====================================================================
+                                    THE RULES
+      =====================================================================*/
+    // Any live cell with fewer than two live neighbours dies (referred to as underpopulation or exposure).
+    // Any live cell with more than three live neighbours dies (referred to as overpopulation or overcrowding).
+    // Any live cell with two or three live neighbours lives, unchanged, to the next generation.
+    // Any dead cell with exactly three live neighbours will come to life.
+    if(cell.state == 1 && cell.aliveNeighbours < 2) {
+      cell.nextState = 0;
+    }
+    else if(cell.state == 1 && cell.aliveNeighbours > 3) {
+      cell.nextState = 0;
+    }
+
+    else if(cell.state == 1 && cell.aliveNeighbours == 2) {
+      cell.nextState = 1;
+    }
+    else if (cell.state == 1 && cell.aliveNeighbours == 3) {
+      cell.nextState = 1;
+    }
+    else if(cell.state == 0 && cell.aliveNeighbours == 3) {
+      cell.nextState = 1;
+    }
+    else{
+      cell.nextState = cell.state;
+    }
+  }, //end
+
+  gameStep: function() {
+    for (var i = 1; i < this.amount*this.amount+1; i++) {
+      // Preventing the function from running if the state stays the same.
+      if (this.cells[i].nextState != this.cells[i].state) {
+        this.setState(this.cells[i], this.cells[i].nextState);
+      }
+    }
+  } // end
+
 }; // end of the grid object
