c768d776dcf8e23a445feeff9bce8d278ab4e146
Jesse <4806@pierson.nl>
2016-06-25 22:18:04 +0200

    cleaning up
    Cleaned up the code a little bit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,59 @@
+function playGame() {
+  for (var i = 1; i < grid.amount*grid.amount+1; i++) {
+    grid.checkNeighbours(grid.cells[i]);
+    grid.nextStates(grid.cells[i]);
+  } // end of for loop
+  grid.gameStep();
+}
+function switchMode(game, interval) {
+  var controls = $('.controls');
+
+  if (controls.hasClass('edit')) {
+    // Pausing the game by clearing the interval.
+    clearInterval(game);
+
+    // Removing the edit class and adding the save class.
+    controls.removeClass('edit');
+    controls.addClass('save');
+    // Emptying the div.
+    controls.empty();
+    // Adding the save icon.
+    controls.append($('<i class="fa fa-play fa-2x"></i>'));
+    $('.trash').append($('<i class="fa fa-trash fa-2x"></i>'));
+  }else {
+    // Starting the game again by re-setting the interval.
+    game = window.setInterval(function() {
+      playGame();
+    },interval); // end of interval
+
+    // Removing the save class and adding the edit class
+    controls.removeClass('save');
+    controls.addClass('edit');
+    // Emptying the div.
+    controls.empty();
+    // Adding the pencil icon.
+    controls.append($('<i class="fa fa-pencil-square fa-2x"></i>'))
+    $('.trash').empty();
+  }
+}
+function switchState(thisCell) {
+  var id = $(thisCell).attr('id');
+  var gcell = grid.cells[id];
+
+  if ($('.controls').hasClass('save')) {
+    if (gcell.state == 1) {
+      grid.setState(gcell, 0);
+    }else {
+      grid.setState(gcell, 1);
+    }
+  }
+}
+function trash() {
+  if ($('.controls').hasClass('save')) {
+    grid.clearGrid();
+  }
+}
+
 function init(interval) {
 
   grid.setAmount(30);
@@ -5,69 +61,19 @@ function init(interval) {
   grid.randomGrid();
   grid.updateDiameter(250);
 
-  var game = window.setInterval(function(){
-    for (var i = 1; i < grid.amount*grid.amount+1; i++) {
-      grid.checkNeighbours(grid.cells[i]);
-      grid.nextStates(grid.cells[i]);
-    } // end of for loop
-    grid.gameStep();
-  },interval); // end of interval
+  var game = window.setInterval(function() {
+    playGame();
+  },interval);
 
   $('.controls').click(function() {
-    var controls = $('.controls');
-
-    if (controls.hasClass('edit')) {
-      // Pausing the game by clearing the interval.
-      clearInterval(game);
-
-      // Removing the edit class and adding the save class.
-      controls.removeClass('edit');
-      controls.addClass('save');
-      // Emptying the div.
-      controls.empty();
-      // Adding the save icon.
-      controls.append($('<i class="fa fa-play fa-2x"></i>'));
-      $('.trash').append($('<i class="fa fa-trash fa-2x"></i>'));
-    }else {
-      // Starting the game again by re-setting the interval.
-      game = window.setInterval(function(){
-        for (var i = 1; i < grid.amount*grid.amount+1; i++) {
-          grid.checkNeighbours(grid.cells[i]);
-          grid.nextStates(grid.cells[i]);
-        } // end of for loop
-        grid.gameStep();
-      },interval); // end of interval
-
-      // Removing the save class and adding the edit class
-      controls.removeClass('save');
-      controls.addClass('edit');
-      // Emptying the div.
-      controls.empty();
-      // Adding the pencil icon.
-      controls.append($('<i class="fa fa-pencil-square fa-2x"></i>'))
-      $('.trash').empty();
-    }
+    switchMode(game, interval)
   });
-
-  var cell = $('.cell');
-  cell.click(function() {
-    var id = $(this).attr('id');
-    var gcell = grid.cells[id];
-
-    if ($('.controls').hasClass('save')) {
-
-      if (gcell.state == 1) {
-        grid.setState(gcell, 0);
-      }else {
-        grid.setState(gcell, 1);
-      }
-    }
-
+  $('.cell').click(function(){
+    var thisCell = this;
+    switchState(thisCell);
   });
   $('.trash').click(function() {
-    if ($('.controls').hasClass('save')) {
-      grid.clearGrid();
-    }
+    trash();
   });
 
 } // end of function
