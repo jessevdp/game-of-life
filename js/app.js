ef8a1cdaed8b319ae73801918ff2e036179a03e6
Jesse <4806@pierson.nl>
2016-06-25 22:50:01 +0200

    fixing things

--- js/app.js
+++ js/app.js
@@ -5,13 +5,10 @@ function playGame() {
   } // end of for loop
   grid.gameStep();
 }
-function switchMode(game, interval) {
+function switchMode() {
   var controls = $('.controls');
 
   if (controls.hasClass('edit')) {
-    // Pausing the game by clearing the interval.
-    clearInterval(game);
-
     // Removing the edit class and adding the save class.
     controls.removeClass('edit');
     controls.addClass('save');
@@ -20,12 +17,9 @@ function switchMode(game, interval) {
     // Adding the save icon.
     controls.append($('<i class="fa fa-play fa-2x"></i>'));
     $('.trash').append($('<i class="fa fa-trash fa-2x"></i>'));
-  }else {
-    // Starting the game again by re-setting the interval.
-    game = window.setInterval(function() {
-      playGame();
-    },interval); // end of interval
 
+    return 'clear';
+  }else {
     // Removing the save class and adding the edit class
     controls.removeClass('save');
     controls.addClass('edit');
@@ -34,6 +28,8 @@ function switchMode(game, interval) {
     // Adding the pencil icon.
     controls.append($('<i class="fa fa-pencil-square fa-2x"></i>'))
     $('.trash').empty();
+
+    return 'set';
   }
 }
 function switchState(thisCell) {
@@ -54,7 +50,6 @@ function trash() {
   }
 }
 
-<<<<<<< HEAD
 function init(interval) {
 
   grid.setAmount(30);
@@ -65,47 +60,44 @@ function init(interval) {
   var game = window.setInterval(function() {
     playGame();
   },interval);
-=======
-  $('.cell').click( (event) => {
-
-    var id = $(event.target).attr('id');
-    var gcell = grid.cells[id];
-
-    if ($('.controls').hasClass('save')) {
-      if (gcell.state == 1) {
-        grid.setState(gcell, 0);
-      } else {
-        grid.setState(gcell, 1);
-      }
-    }
->>>>>>> origin/development
 
   $('.controls').click(function() {
-    switchMode(game, interval)
+    if(switchMode() === 'clear'){
+      clearInterval(game);
+    }else {
+      game = window.setInterval(function() {
+        playGame();
+      },interval);
+    }
   });
   $('.cell').click(function(){
     var thisCell = this;
     switchState(thisCell);
   });
-
-  // Check if the mouse button is down or not.
-  var isMouseDown = false;
-  $(document).mousedown( () => {
-    isMouseDown = true;
-  })
-  .mouseup( () => {
-    isMouseDown = false;
-  })
-
-  // Trigger click event if the mouse enters the
-  // cell while the mouse button is down.
-  $(".cell").mouseenter((e) => {
-    if (isMouseDown) $(e.target).click();
-  })
-
   $('.trash').click(function() {
     trash();
   });
+
+  // Check if the mouse button is down or not.
+var isMouseDown = false;
+$(document).mousedown( () => {
+  isMouseDown = true;
+})
+.mouseup( () => {
+  isMouseDown = false;
+})
+
+// Trigger click event if the mouse enters the
+// cell while the mouse button is down.
+$(".cell").mouseenter((e) => {
+  if (isMouseDown) $(e.target).click();
+})
+
+$('.trash').click(function() {
+  if ($('.controls').hasClass('save')) {
+    grid.clearGrid();
+  }
+});
 
 } // end of function
 
