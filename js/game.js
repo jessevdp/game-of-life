a86f8901b873f4df3755b838c7fdf533ac77be06
lesander <sander@laarhoven.org>
2016-06-26 13:03:54 +0200

    Rename app.js to game.js & move init to init.js

--- js/game.js
+++ js/game.js
@@ -4,6 +4,15 @@
  * MIT Licensed - GoL.js (c) 2016
  */
 
+ /****************************************************************************************************************
+  *                                                   THE RULES
+  *  - Any live cell with fewer than two live neighbours dies (referred to as underpopulation or exposure).
+  *  - Any live cell with more than three live neighbours dies (referred to as overpopulation or overcrowding).
+  *  - Any live cell with two or three live neighbours lives, unchanged, to the next generation.
+  *  - Any dead cell with exactly three live neighbours will come to life.
+  *
+  ****************************************************************************************************************/
+
 var Game = {};
 Game.settings = {};
 
@@ -154,22 +163,3 @@ Game.registerEvents = function () {
   });
 
 }
-
-/**
- * Initialize game and event listeners on page load.
- */
-$(document).ready( () => {
-  Game.init({ width: 30, height: 30 }, () => {
-    Game.registerEvents();
-  });
-});
-
-
-/****************************************************************************************************************
- *                                                   THE RULES
- *  - Any live cell with fewer than two live neighbours dies (referred to as underpopulation or exposure).
- *  - Any live cell with more than three live neighbours dies (referred to as overpopulation or overcrowding).
- *  - Any live cell with two or three live neighbours lives, unchanged, to the next generation.
- *  - Any dead cell with exactly three live neighbours will come to life.
- *
- ****************************************************************************************************************/
