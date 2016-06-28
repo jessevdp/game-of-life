797229e18a0118079a28203573a5412531eaad4b
Jesse <4806@pierson.nl>
2016-06-28 20:30:30 +0200

    Added LZ-string and rounded off compressing/decompressing proces.
    LZ-string is a js program that compresses and decompresses strings.
    Using this program we can now call
    
    codes.makeCode() &
    codes.compress(code) &
    codes.decompress(compressedCode) &
    codes.readCode(decompressedCode / code)

--- js/codes.js
+++ js/codes.js
@@ -1,6 +1,19 @@
 var codes = {};
 
-var makeCode = function() {
+
+/**
+ * Checks if the input is an integer.
+ * Returns true or false.
+ */
+function isInt(n){
+    return Number(n) === n && n % 1 === 0;
+}
+
+/**
+ * Makes a code representing the grids values, creating a long string.
+ * Returns this string.
+ */
+codes.makeCode = function() {
   var c = '';
   for (var i = 1; i < (Grid.amount*Grid.amount)+1; i++) {
     c = c+Grid.cells[i].state.toString()
@@ -8,9 +21,37 @@ var makeCode = function() {
   return c;
 }
 
-var readCode =  function(code) {
-  
+/**
+ * Compresses the code into a string of base64 chars.
+ * Returns the compressed string.
+ */
+codes.compress = function(code) {
+  return LZString.compressToBase64(code);
+}
+
+/**
+ * decompresses the compressed code from a string of base64 chars.
+ * Returns the decompressed string.
+ */
+codes.decompress = function(compressedCode) {
+  return LZString.decompressFromBase64(compressedCode)
+}
+
+/**
+ * Reads a code (representing the grids values) and puts these values back
+ * Into a grid object.
+ * Also checks if this new grids amount of cells == the current grids amount
+ * And if this isn't true then a new grid will be made. (using newAmount function)
+ */
+codes.readCode =  function(code) {
+  // Making sure the code is a string not a number.
+  code = code.toString();
   var amount = Math.sqrt(code.length);
+  // checking if the square root of the codes lenght is an integer.
+  // If not an error is thrown because we can't make half cells
+  if (!isInt(amount)) {
+    throw code.length + ' is not an acceptable length'
+  }
   // Setting the grids amount to the needed amount.
   Game.newAmount(amount);
 
